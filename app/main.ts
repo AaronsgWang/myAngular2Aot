d050508f3566cc564468b82fdacda774e02c253e
nickppa <nickppa@163.com>
2016-12-29 01:32:29 +0800

    not

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { decorateModuleRef } from './environment';
-// import { platformBrowser }    from '@angular/platform-browser';
-import { AppModule } from './app.module';
-
-// platformBrowser().bootstrapModule(AppModule);
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .then(decorateModuleRef)
-    .catch(err => console.error(err));
